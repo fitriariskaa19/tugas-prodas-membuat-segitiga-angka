1a065f8a68c4be4fe70ae9b07097c46cdd0c6c23
fitriariskaa19 <72713210+fitriariskaa19@users.noreply.github.com>
2020-10-26 19:16:35 +0700

    Add files via upload

---
+++ tugas prodas no.7.cpp
@@ -0,0 +1,32 @@
+/* Membuat Tampilan Segitiga Angka */
+/* Fitri Ariska */
+/* 20051397082 */
+/* 2020B */
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main() {
+int n,i,j;
+cout<<"Program Segitiga Angka --> Angka Segitiga \n\n";
+cout<<"Masukkan Segitiga Angka : ";
+cin>>n;
+cout<<"\n";
+for (i=1; i<=n; i++)
+{
+ for (j=1; j<=i; j++)
+{
+  cout<<j;
+}
+cout<<" \n";
+} 
+for (i=n-1; i>=1; i--)
+{
+for(j=1; j<=i;j++)
+{
+ cout<<j;
+}
+cout<<" \n";
+}
+getch ();
+    
+}
